fd41641a05a0271b84f1ce6b6dc966ed12493b07
Miguel <migs.a.vargas@gmail.com>
2020-07-15 12:54:23 -0400

    Add wowtoken models.

---
+++ src/app/shared/models/wow-token-response.ts
@@ -0,0 +1,5 @@
+import { WowToken } from './wow-token';
+
+export interface WowTokenResponse {
+    wowTokens: WowToken[];
+}
---
+++ src/app/shared/models/wow-token.ts
@@ -0,0 +1,5 @@
+export interface WowToken {
+    region: string;
+    price: string;
+    lastUpdatedTimestamp: string;
+}
