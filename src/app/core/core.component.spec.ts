6773cc8104684b6e946b337dcece0b78bf46f9b5
Miguel <migs.a.vargas@gmail.com>
2020-07-16 17:29:53 -0400

    Add dotnetcore tokens component to declarations and add http client testing module to imports of core component test file.

--- src/app/core/core.component.spec.ts
+++ src/app/core/core.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { CoreComponent } from './core.component';
+import { DotnetCoreTokensComponent } from './components/dotnet-core-tokens/dotnet-core-tokens.component';
 
 describe('CoreComponent', () => {
   let component: CoreComponent;
@@ -8,7 +10,13 @@ describe('CoreComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CoreComponent ]
+      declarations: [
+        CoreComponent,
+        DotnetCoreTokensComponent,
+      ],
+      imports: [
+        HttpClientTestingModule
+      ]
     })
     .compileComponents();
   }));
