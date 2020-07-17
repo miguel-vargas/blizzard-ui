9a0cb27d8d054037dc2388d8331cee6c04716f46
Miguel <migs.a.vargas@gmail.com>
2020-07-16 20:13:55 -0400

    Add unit tests for copper to gold pipe.

--- src/app/shared/pipes/copper-to-gold.pipe.spec.ts
+++ src/app/shared/pipes/copper-to-gold.pipe.spec.ts
@@ -5,4 +5,27 @@ describe('CopperToGoldPipe', () => {
     const pipe = new CopperToGoldPipe();
     expect(pipe).toBeTruthy();
   });
+
+  describe('transform', () => {
+    let pipe: CopperToGoldPipe;
+    
+    beforeEach(() => {
+      pipe = new CopperToGoldPipe();
+    });
+
+    it('transform a string of copper to a number of gold', () => {
+      const copperValue = 590000;
+      const expectedGold = copperValue / 10000;
+
+      const actualGold = pipe.transform(copperValue.toString());
+
+      expect(actualGold).toEqual(expectedGold);
+    });
+
+    it('return NaN for non-number string input', () => {
+      const returnValue = pipe.transform("TEST");
+
+      expect(returnValue).toEqual(NaN);
+    });
+  });
 });
