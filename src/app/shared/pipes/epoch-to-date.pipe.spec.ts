457f71f31b21780585226ca083049f3b13c53269
Miguel <migs.a.vargas@gmail.com>
2020-07-20 23:42:31 -0400

    Add unit tests for epoch to date pipe.

--- src/app/shared/pipes/epoch-to-date.pipe.spec.ts
+++ src/app/shared/pipes/epoch-to-date.pipe.spec.ts
@@ -5,4 +5,27 @@ describe('EpochToDatePipe', () => {
     const pipe = new EpochToDatePipe();
     expect(pipe).toBeTruthy();
   });
+
+  describe('transform', () => {
+    let pipe: EpochToDatePipe;
+    
+    beforeEach(() => {
+      pipe = new EpochToDatePipe();
+    });
+
+    it('transform a date string to a date object', () => {
+      const dateValue = 1595301330000;
+      const expectedDate = new Date(dateValue);
+
+      const actualDate = pipe.transform(dateValue.toString());
+
+      expect(actualDate.getTime()).toEqual(expectedDate.getTime());
+    });
+
+    it('return Date(Invalid Date) for non-number string input', () => {
+      const returnValue = pipe.transform("TEST");
+
+      expect(isNaN(returnValue.getTime())).toBeTrue();
+    });
+  });
 });
