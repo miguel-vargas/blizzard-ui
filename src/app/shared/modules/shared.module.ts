5e3ba169eb637a682301ccdb05ba38d8ec0a83c0
Miguel <migs.a.vargas@gmail.com>
2021-03-04 00:26:32 -0500

    Remove material module, moving to prime ng

---
+++ src/app/shared/modules/shared.module.ts
@@ -0,0 +1,13 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  exports: [
+    HttpClientModule,
+    FlexLayoutModule,
+  ]
+})
+export class SharedModule { }
