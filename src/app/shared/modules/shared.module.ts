3b2cf18e00cb755069e3ad155ea75c3da3a3183c
Miguel <migs.a.vargas@gmail.com>
2021-03-05 00:57:33 -0500

    remove flex module and mat icons.

--- src/app/shared/modules/shared.module.ts
+++ src/app/shared/modules/shared.module.ts
@@ -1,12 +1,10 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { PrimeNgModule } from './prime-ng/prime-ng.module';
 
 @NgModule({
   exports: [
     HttpClientModule,
-    FlexLayoutModule,
     PrimeNgModule
   ]
 })
