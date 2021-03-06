3b203489f1e2dcedaf175686192a7de234a86b39
Miguel <migs.a.vargas@gmail.com>
2021-03-06 17:22:12 -0500

    Import prime ng toolbar

--- src/app/shared/modules/prime-ng/prime-ng.module.ts
+++ src/app/shared/modules/prime-ng/prime-ng.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 
 import { CardModule } from 'primeng/card';
 import { SkeletonModule } from 'primeng/skeleton';
+import { ToolbarModule } from 'primeng/toolbar';
 
 @NgModule({
   exports: [
     CardModule,
-    SkeletonModule
+    SkeletonModule,
+    ToolbarModule
   ]
 })
 export class PrimeNgModule { }
