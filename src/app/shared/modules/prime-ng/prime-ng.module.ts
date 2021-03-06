44e82251bfcdc19a79c41db1088a0412c9f82d2c
Miguel <migs.a.vargas@gmail.com>
2021-03-06 01:43:49 -0500

    bring in skeleton module

--- src/app/shared/modules/prime-ng/prime-ng.module.ts
+++ src/app/shared/modules/prime-ng/prime-ng.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 
-import {CardModule} from 'primeng/card';
+import { CardModule } from 'primeng/card';
+import { SkeletonModule } from 'primeng/skeleton';
 
 @NgModule({
   exports: [
-    CardModule
+    CardModule,
+    SkeletonModule
   ]
 })
 export class PrimeNgModule { }
