1ef418dc3609b9ce2d118f09a8a56821d5a169f5
Miguel <migs.a.vargas@gmail.com>
2021-03-10 01:07:40 -0500

    Move out skeleton card component.

---
+++ src/app/features/wow-token/components/loading-card/loading-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'migs-tech-loading-card',
+  templateUrl: './loading-card.component.html',
+  styleUrls: ['./loading-card.component.scss']
+})
+export class LoadingCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
