871d14af58b0d2b4f21b7a4072ead2e67b7886cb
Miguel <migs.a.vargas@gmail.com>
2021-03-10 01:08:30 -0500

    Add components to module declarations.

--- src/app/features/wow-token/wow-token.module.ts
+++ src/app/features/wow-token/wow-token.module.ts
@@ -7,13 +7,19 @@ import { WowTokenRoutingModule } from './wow-token-routing.module';
 import { CopperToGoldPipe } from './pipes/copper-to-gold.pipe';
 import { EpochToDatePipe } from './pipes/epoch-to-date.pipe';
 import { WowTokenComponent } from './components/wow-token.component';
+import { WowTokenCardComponent } from './components/wow-token-card/wow-token-card.component';
+import { LoadingCardComponent } from './components/loading-card/loading-card.component';
+import { ErrorPanelComponent } from './components/error-panel/error-panel.component';
 
 @NgModule({
   declarations: [
     CopperToGoldPipe,
     EpochToDatePipe,
     
-    WowTokenComponent
+    WowTokenComponent,
+    WowTokenCardComponent,
+    LoadingCardComponent,
+    ErrorPanelComponent
   ],
   imports: [
     CommonModule,
